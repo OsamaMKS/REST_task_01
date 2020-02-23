59573f081e55b922602d73fa115563fec83cd955
Osama Sider <osamamksider@hotmail.com>
2020-02-23 14:50:35 +0200

    Task 1 Done

--- flights/models.py
+++ flights/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
 class Flight(models.Model):
 	destination = models.CharField(max_length=100)
 	time = models.TimeField()
--- flights/views.py
+++ flights/views.py
@@ -1,1 +1,14 @@
 from .models import Flight, Booking
+from rest_framework.generics import ListAPIView
+from django.shortcuts import render
+from datetime import datetime
+
+from .serializers import FligthSerializer, BookingSerializer
+
+class FlightsList(ListAPIView):
+    queryset = Flight.objects.all()
+    serializer_class = FligthSerializer
+
+class BookingList(ListAPIView):
+    queryset = Booking.objects.filter(date__gt=datetime.today())
+    serializer_class = BookingSerializer
